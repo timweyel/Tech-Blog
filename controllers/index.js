2fcc0c55d08d12f2a512ba88eea27f8c9c3c484d
TimWeyel <tweyel@gmail.com>
2021-05-17 13:34:37 -0700

    added User model

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,7 @@
 const router = require('express').Router();
-//const apiRoutes = require('./api');
 
-// router.use('/api', apiRoutes);
+const homeRoutes = require('./home-routes.js');
 
-// router.use((req, res) => {
-//   res.send("<h1>Wrong Route!</h1>")
-// });
+router.use('/', homeRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,21 @@
+const path = require('path');
+
 const express = require('express');
 const sequelize = require('./config/connection');
+const routes = require('./controllers/');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
