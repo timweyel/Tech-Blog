70b73275e24c796e7f6b7bd96b44e818f7b3f523
TimWeyel <tweyel@gmail.com>
2021-05-17 14:32:06 -0700

    added user-routes

--- server.js
+++ server.js
@@ -1,23 +1,17 @@
-const path = require('path');
-
 const express = require('express');
+const routes = require('./routes');
 const sequelize = require('./config/connection');
-const routes = require('./controllers/');
-const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({});
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('./controllers/'));
 
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+  app.listen(PORT, () => console.log('Now listening'));
 });
