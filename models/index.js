b73a4e165cc9157cae9ceb5e2801ecda91a34bb7
TimWeyel <tweyel@gmail.com>
2021-05-18 20:15:30 -0700

    added Comment model and routes. updated user and post routes

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require("./User");
 const Post = require("./Post");
+const Comment = require("./Comment");
 
 // create associations
 User.hasMany(Post, {
@@ -7,7 +8,23 @@ User.hasMany(Post, {
   });
 
 Post.belongsTo(User, {
-foreignKey: 'user_id',
-});
+    foreignKey: 'user_id',
+    });
 
-module.exports = { User, Post };
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+    });
+  
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+    });
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+    });
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+    });
+
+module.exports = { User, Post, Comment };
--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,23 +1,34 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Comment } = require('../../models');
+const sequelize = require('sequelize');
 
 // get all users
 router.get('/', (req, res) => {
   Post.findAll({
-    attributes: ['id', 'post_url', 'title', 'created_at'],
-    order: [['created_at', 'DESC']], 
+    order: [['created_at', 'DESC']],
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
     include: [
+      // include the Comment model here:
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
       {
         model: User,
         attributes: ['username']
       }
     ]
-  })
-    .then(dbPostData => res.json(dbPostData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+   })
 });
 
 router.get('/:id', (req, res) => {
@@ -28,8 +39,16 @@ router.get('/:id', (req, res) => {
     attributes: ['id', 'post_url', 'title', 'created_at'],
     include: [
       {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
         model: User,
-        attributes: ['username']
+       attributes: ['username']
+     }
+   },
+      {
+        model: User,
+        attributes: ['username'], 
       }
     ]
   })
